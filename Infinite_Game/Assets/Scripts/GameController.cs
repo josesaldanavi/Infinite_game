9e2955e389f80d06ce2bbf7c24bc47ca5174bf4b
Jose Saldaña <josesaldanavi@gmail.com>
2019-08-30 14:02:14 -0500

    noselol

---
+++ Infinite_Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public GameObject gameOverText;
+    public bool gameOver;
+
+    private void Awake()
+    {
+        //singleton
+        if (GameController.instance == null)
+        {
+            GameController.instance = this;
+        }else if(GameController.instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void EstadoMuerto()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (GameController.instance == this)
+        {
+            GameController.instance = null;
+        }
+    }
+}
---
+++ Infinite_Game/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+
+    private bool isDead = false;
+    private Rigidbody2D rgb2D;
+    public float upForce;
+    private Animator animationPlayer;
+    private void Awake()
+    {
+        //tomar el rigidbody del personaje
+        rgb2D = GetComponent<Rigidbody2D>();
+        animationPlayer = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void  Update()
+    {
+        if (isDead == false)
+        {
+            movementPlayer();
+        }
+    }
+
+    private void  movementPlayer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //frenar en picada
+            rgb2D.velocity = Vector2.zero;
+            //añadir fuerza en el eje y
+            rgb2D.AddForce(Vector2.up * upForce);
+            animationPlayer.SetTrigger("clic");
+        }
+    }
+     
+    //cuando colisione con cualquier collider
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        animationPlayer.SetBool("is_dead",true);
+        //gameController.EstadoMuerto();
+        GameController.instance.EstadoMuerto();
+    }
+}
